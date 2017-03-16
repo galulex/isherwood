2bdeaa28a7bb6cfec41ebd7cfd2408a1cecf3dd7
anoherhello <samuel@samuelseah.com>
2017-03-16 06:36:00 +0000

    Profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,15 +4,15 @@ def render_follow_conditionally
   if user_signed_in?
     follow_toggler 
     else
-    button_to "Follow", new_user_session_path, class: 'btn btn-lake'
+    button_to "Follow", new_user_session_path, class: 'follow-button btn btn-lake'
   end
 end
 
 def follow_toggler
   if current_user.following? @user
-    button_to "Following", unfollow_user_path(@user), class: 'btn btn-default btn-lakeside'
+    button_to "Following", unfollow_user_path(@user), class: 'btn follow-button btn-lake'
     elsif current_user.id != @user.id
-    button_to "Follow", follow_user_path(@user), class: 'btn btn-lake'
+    button_to "Follow", follow_user_path(@user), class: 'btn follow-button btn-lakeside'
     end
   end
 end
