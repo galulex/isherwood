c402794d778ae1c3de9e03b1b2203e79a837fd87
anoherhello <samuel@samuelseah.com>
2017-03-19 12:55:57 +0000

    Change user reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @reviews = current_user.reviews
+    @reviews = @user.reviews
   end
   
   def follow
