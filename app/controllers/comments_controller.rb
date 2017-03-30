e67e2b2f9a3f4380a81918c3cc2d4a126f7a71a9
anoherhello <samuel@samuelseah.com>
2017-03-30 13:43:15 +0000

    Comment remove working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,10 +16,16 @@ class CommentsController < ApplicationController
     end 
     
     def destroy
+      @review = Review.find(params[:review_id]) 
       @comment = Comment.find(params[:id])
       @comment.destroy 
-      redirect_to :back
-    end 
+
+      respond_to do |format|
+      format.html {redirect_to review_path(id: params[:review_id])}
+      format.js 
+    end
+end
+
 
     private 
 
