eebcf094de9c6b45712c91d8c27961861c3d5c7c
anoherhello <samuel@samuelseah.com>
2017-04-01 07:14:17 +0000

    Activity feed design

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user, owner_type: "User")
+    @activities = PublicActivity::Activity.order("created_at desc").where.not(owner_id: current_user)
   end
 end
