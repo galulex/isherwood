72f5ed262d79d78b0168ce02aae5f11d7a9ba604
anoherhello <samuel@samuelseah.com>
2017-04-06 06:55:20 +0000

    Update mobile menu

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,10 +5,13 @@ module SearchHelper
     end
         
     def render_results_count
-      if number_of_results.zero?
+      if params[:term].blank? 
+      elsif number_of_results.zero?
         'NO REVIEWS FOUND'
       else
         "#{number_of_results} #{'review'.pluralize(number_of_results).upcase} FOUND"
       end
+
     end
+    
 end
